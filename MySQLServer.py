8c7303eaf860e3dc74e041465272abcaa8a67253
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2025-12-14 19:41:03 +0000

    week8 task2 fix2

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,15 +16,15 @@ class DatabaseAlreadyExist(Exception):
     def __str__(self):
         return f'{self.name} has already been created'
 
-mycursor.execute("SHOW DATABASES")
-
 db = 'alx_book_store'
 
-# Extract database names from tuples
-databases = [database[0] for database in mycursor.fetchall()]
-
-if db in databases:
-    raise DatabaseAlreadyExist(db)
-else:
+try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print('Database created successfully!')
+except mysql.connector.Error as err:
+    # Error code 1007 means database already exists
+    if err.errno == 1007:
+        raise DatabaseAlreadyExist(db)
+    else:
+        raise
+
